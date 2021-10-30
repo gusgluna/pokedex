9ca484a833ee37775cf8bc6715722013ee5cbba6
gusgluna <gusgluna@gmail.com>
2021-10-30 00:01:06 -0500

    script & html changes

--- pokeScript.js
+++ pokeScript.js
@@ -1,60 +1,38 @@
-// Get data of the pokedex from the API https://pokeapi.co/api/v2/
-async function getPokeData(id) {
+async function getDescription(id) {
   let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  let pokeRes = await res.json();
-  let pokeInfo = {
-    name: pokeRes.name,
-    id: pokeRes.id,
-    type: pokeRes.types[0].type.name,
-    sprite: pokeRes.sprites.front_default,
-  };
-  return pokeInfo;
-}
-
-async function getFullDescription(id) {
-  let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  let fullDesc = await res.json();
-  let res2 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+  let data = await res.json();
+  let res2 = await fetch(`${data.species.url}`);
   let forms = await res2.json();
-
-  let pokeforms = {};
+  let formsObj = {};
   forms.varieties.forEach((form) => {
-    pokeforms[form.pokemon.name] = form.pokemon.url;
+    formsObj[form.pokemon.name] = form.pokemon.url;
   });
 
-  let pokeFullDescription = {
-    name: fullDesc.name,
-    id: fullDesc.id,
+  let pokemonInfo = {
+    name: data.name,
+    id: data.id,
+    artwork: data.sprites.other["official-artwork"].front_default,
+    sprite: data.sprites.front_default,
+    height: data.height / 10,
+    weight: data.weight / 10,
     type:
-      typeof fullDesc.types[1] == "undefined"
-        ? [fullDesc.types[0].type.name]
-        : [fullDesc.types[0].type.name, fullDesc.types[1].type.name],
-    artwork: fullDesc.sprites.other["official-artwork"].front_default,
-    height: fullDesc.height / 10,
-    weight: fullDesc.weight / 10,
-    forms: pokeforms,
+      typeof data.types[1] == "undefined"
+        ? [data.types[0].type.name]
+        : [data.types[0].type.name, data.types[1].type.name],
+    forms: formsObj,
   };
-  console.log(pokeFullDescription);
-  return pokeFullDescription;
+  console.log(pokemonInfo);
+  return pokemonInfo;
 }
 
-async function getPokeForm() {}
-
-// Select all Region Div
+// Select all Region divs and add EventListener to get dex of the region
 const pokeRegions = document.querySelectorAll(".pokeRegion");
-
-// Add a event listener to each Region Div
 pokeRegions.forEach((region) => region.addEventListener("click", selectRegion));
 
-// This function execute when a region is selected
 async function selectRegion() {
-  // First reset all the Region Divs to remove class: "selectedRegion"
   pokeRegions.forEach((region) => region.classList.remove("selectedRegion"));
-  // Then aply the class: "slectedRegion" to the current selected region
   this.classList.toggle("selectedRegion");
-  // define a constant tath get the id of the region.
   const pokeId = this.id;
-  // swicht to execute the funcition getPokedex() acord to selected region.
   switch (pokeId) {
     case "kanto":
       await getPokedex(151, 0);
@@ -84,25 +62,22 @@ async function selectRegion() {
 }
 
 // function to get all pokemon info of a region and input on the pokeBox div
-async function getPokedex(pokeLimit, pokeOffset) {
+async function getPokedex(limit, offset) {
   // while wait to fetch all the info show a loading animation
   document.getElementById("pokeBox").innerHTML =
     '<div class="lds-dual-ring"></div>';
-  //Define a variable to store all of each pokemon of the region.
+  //Define a variable to store all pokemon of current dex.
   let output = "";
-  //for loop to get all the info of every pokemon
-  for (var i = pokeOffset + 1; i <= pokeLimit + pokeOffset; i++) {
-    let currentPokemon = await getPokeData(i);
-    // every iteration update the output varible to store new pokemon data
+  for (var i = offset + 1; i <= limit + offset; i++) {
+    let currentPokemon = await getDescription(i);
     output += `
-    <div class="pokeCard ${currentPokemon.type}" id="${currentPokemon.id}">
+    <div class="pokeCard ${currentPokemon.type[0]}" id="${currentPokemon.id}">
       <img class="sprite" src="${currentPokemon.sprite}">
       <div class="pokeDesc">
         <p>No. ${currentPokemon.id}</p>
         <P><span class="pokeName">${currentPokemon.name}</span></P>
       </div>
-    </div>
-          `;
+    </div>`;
   }
   //insert all the information in the div pokeBox
   document.getElementById("pokeBox").innerHTML = output;
@@ -115,28 +90,26 @@ async function getPokedex(pokeLimit, pokeOffset) {
 
 async function pokemonDescription() {
   const pokemonId = this.id;
-  const pokeDescription = await getFullDescription(pokemonId);
-  let descOutput = `
-  <img 
-  class="artwork ${pokeDescription.type[0]}" 
-  id="pokeArtwork" 
-  src="${pokeDescription.artwork}
-  "/>
-  <div class="description">
-    <p>No. ${pokeDescription.id}</p>
-    <P>Name: <span class="pokeName">${pokeDescription.name}</span></P>
-    <p>Type: ${
-      typeof pokeDescription.type[1] == "undefined"
-        ? `<span class="${pokeDescription.type[0]} typeBadge">${pokeDescription.type[0]}</span>`
-        : `<span class="${pokeDescription.type[0]} typeBadge">${pokeDescription.type[0]}</span> <span class="${pokeDescription.type[1]} typeBadge">${pokeDescription.type[1]}</span>`
-    }</p>
-    <p>Heigth: ${pokeDescription.height} mts.</p>
-    <p>Weigth: ${pokeDescription.weight} kg</p>
-  </div>
-  `;
-
-  document.getElementById("pokeDescription").innerHTML = descOutput;
+  const pokeDescription = await getDescription(pokemonId);
+  document.getElementById("pokeArtwork").src = pokeDescription.artwork;
+  document.getElementById("pokeArtwork").className = "";
+  document
+    .getElementById("pokeArtwork")
+    .classList.add("artwork", `${pokeDescription.type[0]}`);
+  document.getElementById("name").innerHTML = pokeDescription.name;
+  document.getElementById("number").innerHTML = `No. ${pokeDescription.id}`;
+  document.getElementById(
+    "height"
+  ).innerHTML = `Heigth: ${pokeDescription.height} mts.`;
+  document.getElementById(
+    "weight"
+  ).innerHTML = `Weight: ${pokeDescription.weight} kg.`;
+  document.getElementById("typeBadges").innerHTML = `<p>Type: ${
+    typeof pokeDescription.type[1] == "undefined"
+      ? `<span class="${pokeDescription.type[0]} typeBadge">${pokeDescription.type[0]}</span>`
+      : `<span class="${pokeDescription.type[0]} typeBadge">${pokeDescription.type[0]}</span> <span class="${pokeDescription.type[1]} typeBadge">${pokeDescription.type[1]}</span>`
+  }</p>`;
 }
 
-//to init the pokedex show the firs 151 pokemons
+//to init the pokedex show the first 151 pokemons
 getPokedex(10, 0);
